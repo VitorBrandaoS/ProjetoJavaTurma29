f08e7eda6891c0878f6aacea9c34b8aac07e7ec7
jucaravetto <julianna.caravetto@gmail.com>
2021-08-10 19:58:21 -0300

    Montando as opções de menu

--- src/ProjetoJavaGNB/Teste.java
+++ src/ProjetoJavaGNB/Teste.java
@@ -12,6 +12,34 @@ public class Teste {
 		System.out.println("2 para aprender a organizar suas finanças");
 		System.out.println("3 para juntar dinheiro");
 		System.out.println("0 para encerrar o programa");
+		
+		//fazendo as opções do usuário
+		
+		//aqui vai ser necessário adicionar uma leitura de opção
+		//opção = escolha (do usuário de acordo com menu)
+		switch(escolha) //escolha entre 0,1,2 e 3
+		{
+		case 1:
+			//ir para opção quitar dividas;
+			//break;
+		case 2:
+			//ir para opção aprender a organizar finanças;
+			//break;
+		case 3:
+			//ir para opção juntar dinheiro;
+			//break;
+		case 0:
+			System.out.println("Você encerrou o programa, até mais!");
+			//break;
+		default:
+			System.out.println("Por favor, digite uma opção váliida: ");	
+			System.out.println("Por favor, selecione uma opção no menu: ");
+			System.out.println("\n1 para quitar dívidas");
+			System.out.println("2 para aprender a organizar suas finanças");
+			System.out.println("3 para juntar dinheiro");
+			System.out.println("0 para encerrar o programa");
+		}
+		
 	}
 
 }
