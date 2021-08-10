00c420d58987de0e1e11aedbd085c08ed5afd3bf
jucaravetto <julianna.caravetto@gmail.com>
2021-08-10 19:30:26 -0300

    Criando o menu do aplicativo

--- src/ProjetoJavaGNB/Teste.java
+++ src/ProjetoJavaGNB/Teste.java
@@ -3,9 +3,15 @@ package ProjetoJavaGNB;
 public class Teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Ola Mundo!");
-		System.out.println("Teste commit!");
+
+		//Teste do menu
+		System.out.println("Bem-vindo!");
+		System.out.println("\nDe agora em diante você vai ter nossa ajuda para cuidar das suas finanças!");
+		System.out.println("Por favor, selecione uma opção no menu: ");
+		System.out.println("\n1 para quitar dívidas");
+		System.out.println("2 para aprender a organizar suas finanças");
+		System.out.println("3 para juntar dinheiro");
+		System.out.println("0 para encerrar o programa");
 	}
 
 }
