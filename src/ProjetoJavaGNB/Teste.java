8cdcb79618e96f48b85f388aedd7e79c28738fc5
jucaravetto <julianna.caravetto@gmail.com>
2021-08-10 19:10:28 -0300

    Teste commit

--- src/ProjetoJavaGNB/Teste.java
+++ src/ProjetoJavaGNB/Teste.java
@@ -5,6 +5,7 @@ public class Teste {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ola Mundo!");
+		System.out.println("Teste commit!");
 	}
 
 }
